4de630a9263a4f5f01d4fdf91b0e84d4c5d417c4
Jonah Stiennon <jonahss@gmail.com>
2016-07-01 15:51:09 -0700

    use native promises. now no deps!

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-var Q = require('q');
-
 module.exports = telme = function telme(question) {
  var stdin = process.stdin, stdout = process.stdout;
- var deferred = Q.defer();
 
  stdin.resume();
  stdout.write(question + ": ");
 
- stdin.once('data', function(data) {
-   data = data.toString().trim();
-   stdin.pause();
-   deferred.resolve(data);
- });
-
- return deferred.promise;
+ return new Promise((resolve, reject) => {
+   stdin.once('data', function(data) {
+     data = data.toString().trim();
+     stdin.pause();
+     resolve(data);
+   });
+  });
 };
